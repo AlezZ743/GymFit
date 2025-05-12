f79c1f48717ab9138131d73f0c63ad67ac7b61ff
victor <vmgt100@hotmail.com>
2025-05-12 12:48:11 +0200

    modified:   aplicacion.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,58 @@
 $(document).ready(function () {
 
-    $("#respuestas").append("<option selected><p>18</p></option>");
-    
-    for (var i = 18; i <= 100; i++) {
+    let punteros = 0;
+    const secciones = $(".aplicacion-main .app-entrenamiento section");
+    const totalsecciones = secciones.length;
+
+    console.log(secciones);
+    console.log(totalsecciones);
+
+    // Funciones Globales
+    function botonera() {
+        if (punteros == 0) {
+            $("#btnAtras").hide();
+        };
+
+        if (punteros != 0) {
+            $("#btnAtras").show();
+        };
+
+    };
+
+    // Oculto todas la secciones
+    secciones.hide();
+
+    // Relleno el campo de edades de la seccion edad
+    $("#respuestas").append("<option selected><p>8</p></option>");
+    for (var i = 9; i <= 100; i++) {
         var rellenop = "<option>" + i + "</option>";
         $("#respuestas").append(rellenop);
     };
 
-    
+    // Muestro solo la seccion edad
+    $(secciones).eq(0).show();
+
+    $("#btnSiguiente").click(function () {
+        punteros = punteros + 1;
+        console.log(punteros);
+        console.log("Boton Siguiente");
+
+        $(secciones).eq(punteros - 1).hide();
+        $(secciones).eq(punteros).show();
+        botonera();
+    });
+
+    $("#btnAtras").click(function () {
+        punteros = punteros - 1;
+        console.log(punteros);
+        console.log("Boton Atras");
+
+        $(secciones).eq(punteros + 1).hide();
+        $(secciones).eq(punteros).show();
+        botonera();
+    });
+
+
+
 });
 
