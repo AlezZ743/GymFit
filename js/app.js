63f2f7a087b72e4824cd791567d50e6651838a33
victor <vmgt100@hotmail.com>
2025-05-14 09:52:12 +0200

    modified:   aplicacion.html 	modified:   js/app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ $(document).ready(function () {
 
     let puntero = 0;
     const secciones = $("#botones-ejercicios .boton");
-    const h3_ejercicios = $("#mis-ejercicios h3");
     const totalsecciones = secciones.length;
+    var h3_ejercicios = $("#mis-ejercicios h3");
 
     console.log("Secciones: ");
     console.log(secciones);
@@ -14,7 +14,7 @@ $(document).ready(function () {
 
     // Comprueba si se pulsa un boton
     $(secciones).click(function () {
-
+        
         // Ocultamos el boton
         $(this).hide();
 
@@ -23,24 +23,34 @@ $(document).ready(function () {
 
         $("#mis-ejercicios").append($(this).children("h3"));
 
+        // actualizamos variable con los h3
+        
+        h3_ejercicios = $("#mis-ejercicios h3");
+
+        //$("#mis-ejercicios").append($(this).children("*"));
+
     });
 
     $(h3_ejercicios).click(function () {
+        
+        // Busco el boton correspondiente en la tabla de ejercicios
+        console.log("ejercicio pulsado");
+
+        for (var i = 0; i <= totalsecciones; i++) {
+            console.log("Buscando boton");
+            // Si el boton es el buscado, lo muestro
+
+            if (alert($(this).attr("data-ejercicio")) == $(secciones(i).alert("data-ejercicio"))) {
+                console.log("boton encontrado");
+                console.log("i = " + i);
+
+                i = totalsecciones + 1;
+                $(secciones(i)).show();
+            }
+        }
 
         // Oculto el ejercicio seleccionado
         $(this).hide();
-
-        // vuelvo a activar el boton del ejercicio correspondiente
-        for (var i = 0; i <= totalsecciones; i++) {
-            if ($(secciones(i)).children("h3") == $(this.children("h3"))) {
-                puntero = i;
-            };
-        };
-        secciones(puntero).show();
-
-        console.log("Has hecho click en: ");
-        console.log(puntero);
-
     });
 
     // preparar html y js para buscar nombre correspondiente al elemento seleccionado
